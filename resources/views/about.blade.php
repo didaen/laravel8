71679f42e3669d3edee72516cb32377c803063d7
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 07:49:02 +0700

    Pemanggilan data yang dikirim menggunakan key pada halaman view resources/views/about.blade.php.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,8 +9,8 @@
 </head>
 <body>
     <h1>Halaman About</h1>
-    <h3>Dida EN</h3>
-    <h3>allrised@gmail.com</h3>
-    <img src="img/didaen.jpg" alt="Dida EN" width="200">
+    <h3><?= $name; ?></h3>
+    <h3><?= $email; ?></h3>
+    <img src="img/<?= $image; ?>" alt="<?= $name ?>" width="200">
 </body>
 </html>
