f5aa8f8c912aafbb956918ad24f9b0786f7601d2
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 15:07:12 +0700

    Memasukkan variabel blog-post ke dalam Route pada routes/web.php agar dapat dipanggil saat return dan tidak error. blog_posts ini akan dikirimkan ke view Posts

--- routes/web.php
+++ routes/web.php
@@ -28,21 +28,21 @@ Route::get('/about', function () {
     ]);
 });
 
-$blog_post = [
-    [
-        "title" => "Judul Post Pertama",
-        "author" => "Dida EN",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet."
-    ],
-    [
-        "title" => "Judul Post Kedua",
-        "author" => "Yang Jungwon",
-        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet"
-    ]
-];
-
 Route::get('/blog', function () {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "author" => "Dida EN",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "author" => "Yang Jungwon",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet"
+        ]
+    ];
     return view('posts', [
-        "title" => "Blog"
+        "title" => "Blog",
+        "posts" => $blog_post
     ]);
 });
