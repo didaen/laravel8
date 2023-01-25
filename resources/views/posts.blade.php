c69743bb8bed2bb8a89483976ff1441dd6645320
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 14:09:05 +0700

    Menjadikan resources/views/posts.blade.php sebagai child view untuk view main dan menamai section container.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
-    <title>Dida EN | Posts</title>
-</head>
-<body>
-    <h1>Halaman Posts</h1>
+@extends('layouts.main')
 
-</body>
-</html>
+@section('container')
+    <h1>Halaman Posts</h1>
+@endsection
