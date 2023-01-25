c901a847d20548d5587cda64657cce302cd4cf4a
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 15:53:13 +0700

    Secara statis seperti inilah yang ingin ditampilkan pada resources/views/posts.blade.php. Namun nanti akan mengambil dari data yang dikirimkan.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman Posts</h1>
+    <h1>Judul</h1>
+    <h5>Dida EN</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint libero nobis expedita, deserunt cumque nostrum. Quod repellendus eius iure ipsum.</p>
 @endsection
