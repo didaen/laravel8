1867764f1e1b6ca832bee2b144f8b3b395d54682
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 16:38:17 +0700

    Memperbaiki pemanggilan data pada view resources/views/posts.blade.php karena data yang dipanggil merupakan value dari sebuah array, yaitu slug.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
     @foreach ($posts as $post)
         <article class="mb-5">
             <h2>
-                <a href="/posts/{{ $slug }}">{{ $post["title"] }}</a>
+                <a href="/posts/{{ $post["slug"] }}">{{ $post["title"] }}</a>
             </h2>
             <h5>Oleh : {{ $post["author"] }}</h5>
             <p>{{ $post["body"] }}</p>
