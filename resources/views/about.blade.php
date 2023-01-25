181e02fb2f2cfdb98adebdda6021ad279c0c7272
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 14:18:30 +0700

    Memperbaiki isi child view about dengan tetap menggunakan blade template untuk pemanggilan data.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1>Halaman About</h1>  
+    <h1>Halaman About</h1>
+    <h3>{{ $name }}</h3>
+    <p>{{ $email }}</p>
+    <img src="img/{{ $image }}" alt="{{ $name }}" width="200">
 @endsection
