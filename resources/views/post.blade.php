9cfb9e9d69d999e64a4213c08401cb0a21e88d67
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 16:53:21 +0700

    Menambahkan link Back to Post untuk kembali ke route /blog.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,4 +8,6 @@
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Provident dolorum tempora corporis quam reiciendis, officiis enim quibusdam temporibus assumenda tenetur quo? Nostrum, obcaecati. Ab delectus animi magnam fugiat molestiae sapiente quos tempore illo nesciunt aut dolorum rerum deleniti quis cupiditate, veniam cum alias rem autem nulla odio expedita hic sunt.
         </p>
     </article>
+
+    <a href="/blog">Back to Posts</a>
 @endsection
