422673891c178c68d986b45d2694ef2f56fb87eb
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 16:18:40 +0700

    Menambahkan tag HTML5 <article> untuk membungkus postingan pada resources/views/posts.blade.php.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,10 @@
 
 @section('container')
     @foreach ($posts as $post)
-        <h2>{{ $post["title"] }}</h2>
-        <h5>Oleh : {{ $post["author"] }}</h5>
-        <p>{{ $post["body"] }}</p>
+        <article class="mb-5">
+            <h2>{{ $post["title"] }}</h2>
+            <h5>Oleh : {{ $post["author"] }}</h5>
+            <p>{{ $post["body"] }}</p>
+        </article>
     @endforeach
 @endsection
