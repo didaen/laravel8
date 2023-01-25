8d8be61233fa424ededbe0f6be4e0d9116cc5066
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 14:29:55 +0700

    Memotong bagian navbar dan menggantinya dengan @include(folder.nama_partial') yang dapat digunakan sebagai komponen bongkar pasang. Jadi bisa dipanggil sewaktu-waktu.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,27 +8,7 @@
   </head>
   <body>
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container">
-          <a class="navbar-brand" href="#">Dida EN Blog</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" href="/">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/about">About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="/blog">Blog</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    @include('partials.navbar')
       
     <div class="container mt-4">
         <h1>@yield('container')</h1>
