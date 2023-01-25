f79b14af685a0a58e130b78802b36949f39f4695
Dida En <firdanb@students.unnes.ac.id>
2023-01-25 07:50:15 +0700

    Menambahkan tanda ; di pada alt <img> resources/views/about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -11,6 +11,6 @@
     <h1>Halaman About</h1>
     <h3><?= $name; ?></h3>
     <h3><?= $email; ?></h3>
-    <img src="img/<?= $image; ?>" alt="<?= $name ?>" width="200">
+    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" width="200">
 </body>
 </html>
