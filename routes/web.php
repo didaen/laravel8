896e290c57c04976cccdafd64714e16569bca6fe
Dida En <firdanb@students.unnes.ac.id>
2023-02-10 01:16:22 +0700

    Mengimpor controller PostController pada routes/web.php.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
