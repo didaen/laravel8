da19dc0460044094a42e5e5d970514efb638d278
Dida En <firdanb@students.unnes.ac.id>
2023-02-09 14:32:57 +0700

    Membuat method static all() pada app/Models/Post.php untuk mengembalikan property blog_posts yang memiliki access modifier private,

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -19,4 +19,9 @@ class Post
             "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut libero, ipsam nihil odit nam temporibus voluptatem ad. Nobis, adipisci tenetur! Atque dolorem magni at sed voluptas maiores commodi suscipit fuga, voluptatum praesentium, dolor recusandae ullam aut similique culpa, unde cupiditate quaerat! Voluptatem quia dolore fugiat exercitationem et commodi delectus, ipsum quod laborum aperiam est, consectetur consequatur inventore sunt quos sequi magnam? Illum ipsum eaque provident incidunt dicta atque ipsa, quam dolore a. Nulla enim consequuntur sequi eveniet a fugit amet"
         ]
     ];
+
+    public static function all()
+    {
+        return self::$blog_posts;
+    }
 }
