c254d78766aff836aa1a05279a1a8182beaf737a
Dida En <firdanb@students.unnes.ac.id>
2023-02-10 01:22:49 +0700

    Memotong return view dari route /post/{slug} ke PostController.

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,4 @@ Route::get('/about', function () {
 Route::get('/blog', [PostController::class, 'index']);
 
 // HALAMAN SINGLE POST
-Route::get('/posts/{slug}', function ($slug) {
-
-    return view('post', [
-        "title" => "Single Post",
-        "post" => Post::find($slug)
-    ]);
-});
+Route::get('/posts/{slug}', [PostController::class, 'show']);
