5bf6a3e55ed65bcfc505bbe0339c97b4a228fa8d
Dida En <firdanb@students.unnes.ac.id>
2023-02-11 18:11:28 +0700

    Dengan menggunakan ROUTE MODEL BINDING, data yang dikirim melalui URL langsung bisa menjadi suatu syarat yang unik untuk mencari data yang sesuai. {post:slug}. post ini nanti harus sama dengan parameter yang ada di PostController di method show.

--- routes/web.php
+++ routes/web.php
@@ -32,4 +32,4 @@ Route::get('/about', function () {
 Route::get('/blog', [PostController::class, 'index']);
 
 // HALAMAN SINGLE POST
-Route::get('/posts/{slug}', [PostController::class, 'show']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
