213be3c3bd2c12154a6930a52112aa7f3296fba9
Dida En <firdanb@students.unnes.ac.id>
2023-02-14 06:56:36 +0700

    Menambahkan <a> pada bagian category pada resources/views/post.blade.php. Menggunakan template blade dan memanfaatkan slug dari categories sebagai href nya.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>by {{ $post->author }} in {{ $post->category->name }}</h5>
+        <h5>by {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
         
         {!! $post->body !!}
         
