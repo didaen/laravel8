e227195bdea29ad11366edcce59705bd9ec901de
Dida En <firdanb@students.unnes.ac.id>
2023-02-14 23:44:32 +0700

    Jadi penulisan nama function pada saat perelasian itu gak harus sama dengan nama model yg berelasi. Disini contohnya adalah nama modelnya Post namun nama funcitonnya posts pada app/Models/Category.php. Yang paling penting adalah isi dari function itu atau code perelasiannya.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,4 +10,16 @@ class Category extends Model
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    // Method yang mengarah ke Post
+    // Untuk menghubungkan tabel categories dengan posts
+    // Nama methodnya harus sama dengan nama modelnya
+    public function posts ()
+    {
+        // Relasi antara post dengan category apa
+        // Category menitipkan foreignkey ke Post
+        // Berarti pake has
+        // Satu category bisa memiliki  banyak post
+        return $this->hasMany(Post::class);
+    }
 }
