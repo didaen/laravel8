9ab1a0ac1d8c87c4877bb7644ad105c8647a3a33
Dida En <firdanb@students.unnes.ac.id>
2023-02-16 22:42:42 +0700

    Mengubah link yang menampilkan nama author atau user yang menulis suatu postingan karena sudah dilakukan penghubungan 2 tabel. Sehingga href yang dikirim sesuai dengan id user untuk keperluan pensortiran postingan sesuai dengan penulisnya.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,11 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>by {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></h5>
+        <h5>by <a href="/user-{{ $post->user->id }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
         
         {!! $post->body !!}
         
     </article>
 
-    <a href="/blog">Back to Posts</a>
+    <a href="/posts">Back to Posts</a>
 @endsection
