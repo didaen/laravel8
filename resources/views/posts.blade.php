3ddf66714e9c5d562dfa3c5c5baa39529ecd9f88
Dida En <firdanb@students.unnes.ac.id>
2023-02-16 19:56:56 +0700

    Mengubah tampilan link menjadi tidak bertgaris bawah pada resources/views/posts.blade.php dengan menambahkan class 'text-decoration-none' dan menambahkan link Read more... yang membawa ke view single post. Menambahkan tampilan category setelah author.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,12 +4,15 @@
     <h1 class="mb-5">Halaman Blog Posts</h1>
 
     @foreach ($posts as $post)
-        <article class="mb-5">
+        <article class="mb-5 border-bottom pb-4">
             <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <h5>Oleh : {{ $post->author }}</h5>
+            <h5>by <a href="#" class="text-decoration-none">{{ $post->author }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+
             <p>{{ $post->excerpt }}</p>
+
+            <p><a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more...</a></p>
         </article>
     @endforeach
 @endsection
