1863593d18dbdf8ab519efb9288b9aa6b60e641e
Dida En <firdanb@students.unnes.ac.id>
2023-02-16 20:14:33 +0700

    Menambahkan 1 field foreign key user_id pada tabel posts database/migrations/2023_02_10_144335_create_posts_table.php yang akan digunakan untuk menambahkan data author. Authornya akan dibuat ditabel author nantinya.

--- database/migrations/2023_02_10_144335_create_posts_table.php
+++ database/migrations/2023_02_10_144335_create_posts_table.php
@@ -17,6 +17,7 @@ class CreatePostsTable extends Migration
             $table->id();
             $table->string('title');
             $table->foreignId('category_id');
+            $table->foreignId('user_id');
             $table->string('slug')->unique();
             $table->string('author');
             $table->text('excerpt');
