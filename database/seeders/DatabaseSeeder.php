381accd8d2c8b3ee3443fbf439155e4357777a30
Dida En <firdanb@students.unnes.ac.id>
2023-02-18 08:12:37 +0700

    Membuat5  data palsu untuk pengguna menggunakan FACTORY pada database/seeders/DatabaseSeeder.php.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        User::factory(5)->create();
 
         // User::create([
         //     'name' => 'Dida En',
