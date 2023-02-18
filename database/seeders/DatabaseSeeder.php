f31548116dc06fdf125b93d4a4586fbc06b1c88e
Dida En <firdanb@students.unnes.ac.id>
2023-02-18 10:22:51 +0700

    Mengomentari Post::create() pada database/seeders/DatabaseSeeder.php.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -50,31 +50,31 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'category_id' => 1,
-            'user_id' => 1,
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
-            'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
+        //     'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Kedua',
-            'category_id' => 1,
-            'user_id' => 1,
-            'slug' => 'judul-kedua',
-            'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
-            'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Kedua',
+        //     'category_id' => 1,
+        //     'user_id' => 1,
+        //     'slug' => 'judul-kedua',
+        //     'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
+        //     'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ketiga',
-            'category_id' => 3,
-            'user_id' => 2,
-            'slug' => 'judul-ketiga',
-            'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
-            'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ketiga',
+        //     'category_id' => 3,
+        //     'user_id' => 2,
+        //     'slug' => 'judul-ketiga',
+        //     'excerpt' => 'Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque',
+        //     'body' => '<p>Omnis, non quia eum vitae minima nostrum dolorem, quasi cupiditate saepe adipisci cumque nesciunt incidunt reiciendis expedita necessitatibus doloribus qui magnam fugit iste enim! Nesciunt, reprehenderit aliquam dolore, totam harum architecto in recusandae numquam atque maxime doloremque culpa perferendis laudantium eligendi repudiandae sed assumenda explicabo voluptatum aliquid asperiores itaque omnis blanditiis. Illum dolor impedit iure praesentium, distinctio at dolorem debitis consectetur! Culpa, sapiente ea. Maiores.</p><p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Asperiores, nihil? Repellat quae asperiores ipsam qui delectus dolore rerum, minus harum.</p>'
+        // ]);
     }
 }
