e7602558d148ebf7c9bd8384ffa5553f5b330fd6
Dida En <firdanb@students.unnes.ac.id>
2023-02-25 11:26:15 +0700

    Mengganti href pada author mengikuti route yang sudah dibuat dalam web.php '/authors/{author:username}' pada semua view yang menampilkan nama author.

--- resources/views/author.blade.php
+++ resources/views/author.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5">Post by {{ $user }}</h1>
+    <h1 class="mb-5">Post by {{ $author }}</h1>
 
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-4">
             <h2>
                 <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <h5>by <a href="/users/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+            <h5>by <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
 
             <p>{{ $post->excerpt }}</p>
 
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,7 +3,7 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>by <a href="/users/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+        <h5>by <a href="/authors/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
         
         {!! $post->body !!}
         
