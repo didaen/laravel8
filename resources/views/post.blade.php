2bcf503923b5736de15a6738328b90fad03c3f9c
Dida En <firdanb@students.unnes.ac.id>
2023-02-25 08:59:56 +0700

    Memperbaiki tampilan pada resources/views/post.blade.php agar saat tulisan author diklik secara otomatis tersortir oleh nama author. Menambahkan martgin-top pada link 'Back to posts'.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,11 +3,13 @@
 @section('container')
     <article>
         <h2>{{ $post->title }}</h2>
-        <h5>by <a href="/user-{{ $post->user->id }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+        <h5>by <a href="/users/{{ $post->user->username }}" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
         
         {!! $post->body !!}
         
     </article>
 
-    <a href="/posts">Back to Posts</a>
+    <div class="mt-3">
+        <a href="/posts">Back to Posts</a>
+    </div>
 @endsection
