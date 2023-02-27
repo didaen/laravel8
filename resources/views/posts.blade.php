ae1718ee85e7b55f05433122f62164c8b7993d96
Dida En <firdanb@students.unnes.ac.id>
2023-02-27 08:01:13 +0700

    Menuliskan if menggunakan sintaks Blade untuk mengecek ada postingan atau tidak. Kalau tidak ada postingan, akan ditampilkan tulisan 'No post found'. Tampilan akan ditampilkan dengan card Bootstrap.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,14 +3,21 @@
 @section('container')
     <h1 class="mb-5"> {{ $title }} </h1>
 
-    <div class="card mb-3">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+    {{-- IF MENGGUNAKAN BLADE --}}
+    @if ($posts->count())
+        <div class="card mb-3">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+            </div>
         </div>
-      </div>
+    @else
+        <p>No post found</p>
+    @endif
+    {{-- END IF MENGGUNAKAN BLADE --}}
+
 
     @foreach ($posts as $post)
         <article class="mb-5 border-bottom pb-4">
