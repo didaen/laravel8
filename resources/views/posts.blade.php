51466df380f621b8d5c830c1ad971819e1671cfd
Dida En <firdanb@students.unnes.ac.id>
2023-03-16 10:59:27 +0700

    Menambahkan 'text-dark' untuk membuat style judul yang link itu menjadi hitam biasa namun bisa diklik seperti link pada Hero postingan pada resources/views/posts.blade.php.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -9,7 +9,7 @@
             <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
             <div class="card-body text-center">
                 <h3 class="card-title">
-                    <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none">{{ $posts[0]->title }}</a>
+                    <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none text-dark">{{ $posts[0]->title }}</a>
                 </h3>
                 <p>
                     <small class="text-muted">
