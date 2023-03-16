60712efe524e6b694675d865636206f376f0a6dd
Dida En <firdanb@students.unnes.ac.id>
2023-03-16 14:26:49 +0700

    Memperbaiki tampilan saat 1 postingan dipilih menjadi lebih memusat di tengah tulisannya.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,15 +1,21 @@
 @extends('layouts.main')
 
 @section('container')
-    <article>
-        <h2>{{ $post->title }}</h2>
-        <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
-        
-        {!! $post->body !!}
-        
-    </article>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                
+                    <h1 class="mb-5">{{ $post->title }}</h1>
 
-    <div class="mt-3">
-        <a href="/posts">Back to Posts</a>
+                    <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+                    
+                    {!! $post->body !!}
+
+                    <div class="d-block mt-3">
+                        <a href="/posts">Back to Posts</a>
+                    </div>
+            </div>
+        </div>
     </div>
+
 @endsection
