a13ad90d6a0b111370d241b5d2f9131c12c57672
Dida En <firdanb@students.unnes.ac.id>
2023-03-16 10:57:07 +0700

    Menambahkan alt pada <img> sesuai dengan nama kategori postingan pada resources/views/posts.blade.php.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     {{-- IF MENGGUNAKAN BLADE --}}
     @if ($posts->count())
         <div class="card mb-3">
-            <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
+            <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
             <div class="card-body text-center">
                 <h3 class="card-title">
                     <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none">{{ $posts[0]->title }}</a>
