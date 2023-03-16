8f487906243f5e3db8bf4698e3afb6723315b799
Dida En <firdanb@students.unnes.ac.id>
2023-03-16 10:41:43 +0700

    Mengedit hotlink src untuk <img> link Unsplash agar dapat menampilkan gambar sesuai dengan categori postingan dengan menambahkan posts[0]->category->name ke dalam link API unsplash-nya.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,7 +6,7 @@
     {{-- IF MENGGUNAKAN BLADE --}}
     @if ($posts->count())
         <div class="card mb-3">
-            <img src="https://source.unsplash.com/1200x400/?nature,water" class="card-img-top" alt="...">
+            <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="...">
             <div class="card-body text-center">
                 <h3 class="card-title">{{ $posts[0]->title }}</h3>
                 <p>
