8bbbee484bc93bd6d8e5313a2599fdc776d1aa2e
Dida En <firdanb@students.unnes.ac.id>
2023-03-16 14:41:23 +0700

    Mengubah factory untuk body dengan mengubahnya menjadi collection, kemudian map() kemudian dengan arrow function PHP menjadi paragraf. Hasilnya tiap paragraf akan di awali dengan <p> dan diakhiri dengan </p>.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -17,7 +17,9 @@ class PostFactory extends Factory
             'title' => $this->faker->words(mt_rand(2, 8)),
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
-            'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5, 10))) . '</p>',
+            // 'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5, 10))) . '</p>',
+            'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
+                        ->map(fn($p) => "<p>$p</p>"),
             'user_id' => mt_rand(1, 5),
             'category_id' => mt_rand(1, 4)
         ];
