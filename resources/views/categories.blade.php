1ea3048be03340a6a66e1655c12f1df3a522b439
Dida En <firdanb@students.unnes.ac.id>
2023-03-17 14:43:41 +0700

    Membuat tampilan categories menjadi IMAGE OVERLAY BOOTSTRAP.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -3,6 +3,23 @@
 @section('container')
     <h1 class="mb-5">Categories</h1>
 
+    <div class="container">
+        <div class="row">
+            @foreach ($categories as $category)
+                <div class="col-md-4">
+                    <div class="card text-bg-dark">
+                        <img src="..." class="card-img" alt="...">
+                        <div class="card-img-overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+
     @foreach ($categories as $category)
         <ul>
             <li>
