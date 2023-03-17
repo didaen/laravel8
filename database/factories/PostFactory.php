5fc37f058ad086d913f1945005045a21b1c35089
Dida En <firdanb@students.unnes.ac.id>
2023-03-17 11:34:49 +0700

    Pertama, hasil dari map() masih berupa array sehingga harus digabungkan dengan menambahkan implode() pada 'body' dan untuk 'title' berubah dari words menjadi sentence untuk lebih memudahkan karena ketika memakai words maka hasilnya masih berupa array pada database/factories/PostFactory.php.

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -14,12 +14,13 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->words(mt_rand(2, 8)),
+            'title' => $this->faker->sentence(mt_rand(2, 8)),
             'slug' => $this->faker->slug(),
             'excerpt' => $this->faker->paragraph(),
             // 'body' => '<p>' . implode('</p><p>', $this->faker->paragraphs(mt_rand(5, 10))) . '</p>',
             'body' => collect($this->faker->paragraphs(mt_rand(5, 10)))
-                        ->map(fn($p) => "<p>$p</p>"),
+                        ->map(fn($p) => "<p>$p</p>")
+                        ->implode(''),
             'user_id' => mt_rand(1, 5),
             'category_id' => mt_rand(1, 4)
         ];
