c7d38e4cb1c059f73d82f9699593575a350a51ea
Dida En <firdanb@students.unnes.ac.id>
2023-03-17 13:58:10 +0700

    Mengatur margin agar tampilan menjadi lebih pas dan memperbesar font postingan pada resources/views/post.blade.php.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,13 +2,16 @@
 
 @section('container')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center mb-5">
             <div class="col-md-8">
                 
-                    <h1 class="mb-5">{{ $post->title }}</h1>
+                    <h1 class="mb-3">{{ $post->title }}</h1>
 
-                    <h5>by <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
-                    <article>
+                    <h5>By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+                    
+                    <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="img-fluid">
+
+                    <article class="my-3 fs-5">
                         {!! $post->body !!}
                     </article>
 
