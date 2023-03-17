ece1af634f41ee28e9aec527118797169f1c1144
Dida En <firdanb@students.unnes.ac.id>
2023-03-17 15:04:00 +0700

    Mendesain categories sampai selesai dan image overlay-nya bisa diklik menuju kategori yang dicari.

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,26 +6,18 @@
     <div class="container">
         <div class="row">
             @foreach ($categories as $category)
-                <div class="col-md-4">
-                    <div class="card text-bg-dark">
-                        <img src="..." class="card-img" alt="...">
-                        <div class="card-img-overlay">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                        <p class="card-text"><small>Last updated 3 mins ago</small></p>
+                <div class="col-md-4 mb-3">
+                    <a href="/categories/{{ $category->slug }}">
+                        <div class="card text-bg-dark">
+                            <img src="https://source.unsplash.com/500x500?{{ $category->name }}" class="card-img" alt="{{ $category->name }}">
+                            <div class="card-img-overlay d-flex align-items-center p-0">
+                                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.7)">{{ $category->name }}</h5>
+                            </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             @endforeach
         </div>
     </div>
 
-    @foreach ($categories as $category)
-        <ul>
-            <li>
-                <h2><a href="/categories/{{ $category->slug }}">{{ $category->name }}</a></h2>
-            </li>
-        </ul>
-
-    @endforeach
 @endsection
