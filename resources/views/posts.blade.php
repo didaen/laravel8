375be3d0559b2d2ec84ec96f17dfee788844eb0f
Dida En <firdanb@students.unnes.ac.id>
2023-03-18 16:37:00 +0700

    Menambahkan INPUT GROUP BOOTSTRAP untuk membuat kolom pencarian, mengatur margin judul 'All Post' dan mengubahnya menjadi rata tengah, merubah buttonnya menjadi warna biru dengan class 'btn-primary'.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,20 @@
 @extends('layouts.main')
 
 @section('container')
-    <h1 class="mb-5"> {{ $title }} </h1>
+    <h1 class="mb-3 text-center"> {{ $title }} </h1>
+
+
+    <div class="row justify-content-center mb-3">
+        <div class="col-md-6">
+            <form action="/posts">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search.." name="search">
+                    <button class="btn btn-primary" type="submit">Search</button>
+                </div>
+            </form>
+        </div>
+    </div>
+
 
     {{-- IF MENGGUNAKAN BLADE --}}
     @if ($posts->count())
@@ -55,5 +68,5 @@
         <p class="text-center fs-4">No post found</p>
     @endif
     {{-- END IF MENGGUNAKAN BLADE --}}
-    
+
 @endsection
