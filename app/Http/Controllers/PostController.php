26c1532191eef1e20a02eee602c8e956977fb97b
Dida En <firdanb@students.unnes.ac.id>
2023-03-20 14:56:22 +0700

    Memindahkan pengkondisian if(request('search') ke Model Post dari app/Http/Controllers/PostController.php.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -16,13 +16,6 @@ class PostController extends Controller
         // Buat variabel untuk menangkap query dan urutkan dari yang paling baru
         $posts = Post::latest();
 
-        // Jika ada pencarian (sesuatu yang ditulis pada kolom pencarian),
-        if(request('search')) {
-            // maka cari juga
-            $posts->where('title', 'like', '%' . request('search') . '%')
-                  ->orWhere('body', 'like', '%' . request('search') . '%');
-        }
-
         return view('posts', [
             "title" => "All Posts",
             "active" => "blog",
