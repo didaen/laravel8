323e80b4fba0a943b6e2e38f4e7126ced7273090
Dida En <firdanb@students.unnes.ac.id>
2023-03-25 14:55:34 +0700

    Mengganti href authors juga pada resources/views/post.blade.php.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
                 
                     <h1 class="mb-3">{{ $post->title }}</h1>
 
-                    <h5>By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
+                    <h5>By <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></h5>
                     
                     <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}" class="img-fluid">
 
