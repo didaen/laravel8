69df4e72cc668050dbcc460344f81eea30164fdf
Dida En <firdanb@students.unnes.ac.id>
2023-04-01 06:27:45 +0700

    Melakukan perubahan class dari col-md-4 ke col-lg-4 untuk mengatur responsif, perubahan terhadap src <img>, menambahkan class rounded-top dan rounded-bottom untuk mengatur tampilan dari input agar ada lengkungan.

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -2,14 +2,14 @@
 
 @section('container')
     <div class="row justify-content-center">
-        <div class="col-md-4">
+        <div class="col-lg-4">
             <main class="form-registration w-100 m-auto">
                 <form>
-                    <img class="mb-4" src="../img/logopyabumuda.png" alt="Logo Physics Yourself" width="72" height="72">
+                    <img class="mb-4" src="/img/logopyabumuda.png" alt="Logo Physics Yourself" width="72" height="72">
                     <h1 class="h3 mb-3 fw-normal">Registration</h1>
                 
                     <div class="form-floating">
-                        <input type="text" class="form-control" id="name" placeholder="Name">
+                        <input type="text" class="form-control rounded-top" id="name" placeholder="Name">
                         <label for="name">Name</label>
                     </div>
                     <div class="form-floating">
@@ -21,11 +21,11 @@
                         <label for="email">Email</label>
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control" id="password" placeholder="Password">
+                        <input type="password" class="form-control rounded-bottom" id="password" placeholder="Password">
                         <label for="password">Password</label>
                     </div>
                 
-                    <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+                    <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
                 </form>
 
                 <small class="d-block text-center mt-3">Already have an account? <a href="/login">Login</a></small>
