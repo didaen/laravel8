2e2a991dd90965c5005528cffc8d9b44b2ee740b
Dida En <firdanb@students.unnes.ac.id>
2023-03-31 16:39:08 +0700

    Menambahkan link CSS ke /css/style.css pada resources/views/layouts/main.blade.php.

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -11,6 +11,9 @@
     {{-- Bootstrap Icons --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 
+    {{-- My Styles --}}
+    <link rel="stylesheet" href="/css/style.css">
+
   </head>
   <body>
 
