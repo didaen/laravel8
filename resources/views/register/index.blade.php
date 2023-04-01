bc7838e9495d207883293fa4e0f65b1b1bdac84e
Dida En <firdanb@students.unnes.ac.id>
2023-04-01 07:18:05 +0700

    Menambahkan attribute name pada setiap <input> pada form register untuk keperluan pengiriman data.

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -10,19 +10,19 @@
                     <h1 class="h3 mb-3 fw-normal">Registration</h1>
                 
                     <div class="form-floating">
-                        <input type="text" class="form-control rounded-top" id="name" placeholder="Name">
+                        <input type="text" class="form-control rounded-top" id="name" name="name" placeholder="Name">
                         <label for="name">Name</label>
                     </div>
                     <div class="form-floating">
-                        <input type="text" class="form-control" id="username" placeholder="Username">
+                        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
                         <label for="username">Username</label>
                     </div>
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="email" placeholder="Email">
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
                         <label for="email">Email</label>
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control rounded-bottom" id="password" placeholder="Password">
+                        <input type="password" class="form-control rounded-bottom" id="password" name="password" placeholder="Password">
                         <label for="password">Password</label>
                     </div>
                 
