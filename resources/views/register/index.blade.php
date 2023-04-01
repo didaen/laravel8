86c70eba4e04d8593fb1d546c43296e30730655c
Dida En <firdanb@students.unnes.ac.id>
2023-04-01 10:06:57 +0700

    Mencopy paste @error() is-invalid @enderror dan @error() {{ dollarmessage }} @enderror ke semua input.

--- resources/views/register/index.blade.php
+++ resources/views/register/index.blade.php
@@ -19,16 +19,31 @@
                         @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                        <input type="text" class="form-control @error('username') is-invalid @enderror" id="username" name="username" placeholder="Username">
                         <label for="username">Username</label>
+                        @error('username')
+                            <div class="invalid-feedback"> 
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+                        <input type="email" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="Email">
                         <label for="email">Email</label>
+                        @error('email')
+                            <div class="invalid-feedback"> 
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                     <div class="form-floating">
-                        <input type="password" class="form-control rounded-bottom" id="password" name="password" placeholder="Password">
+                        <input type="password" class="form-control rounded-bottom @error('password') is-invalid @enderror" id="password" name="password" placeholder="Password">
                         <label for="password">Password</label>
+                        @error('password')
+                            <div class="invalid-feedback"> 
+                                {{ $message }}
+                            </div>
+                        @enderror
                     </div>
                 
                     <button class="w-100 btn btn-lg btn-primary mt-3" type="submit">Register</button>
