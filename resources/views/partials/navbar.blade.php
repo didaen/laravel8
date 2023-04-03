dbfa8cc3afeb786821f166016c6f8e654edccd32
Dida En <firdanb@students.unnes.ac.id>
2023-04-03 13:14:27 +0700

    Membuat menu dropdown Logout  dengan form saja yang hanya berisi <button>, form itu memiliki action yang mengarah ke '/logout' dan methodnya 'post'. Selain itu pada form harus diberi @csrf.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -27,9 +27,14 @@
                 Welcome back, {{ auth()->user()->name }}
               </a>
               <ul class="dropdown-menu">
-                <li><a class="dropdown-item" href="#"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
+                <li><a class="dropdown-item" href="/dashboard"><i class="bi bi-layout-text-sidebar-reverse"></i> My Dashboard</a></li>
                 <li><hr class="dropdown-divider"></li>
-                <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-right"></i> Logout</a></li>
+                <li>
+                  <form action="/logout" method="post">
+                    @csrf
+                    <button type="submit" class="dropdown-item"><i class="bi bi-box-arrow-right"></i> Logout</button>
+                  </form>
+                </li>
               </ul>
             </li>
           @else
