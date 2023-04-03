c52fc8767656b3c0ddd889461239e7ffcce548e4
Dida En <firdanb@students.unnes.ac.id>
2023-04-03 19:32:50 +0700

    Isi dari <main> pada resources/views/dashboard/layouts/main.blade.php akan berganti sesuai dengan user yang mengakses, maka dari itu menggunakan @yield('container') untuk menampilkan bagian yang dinamis tersebut.

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -75,9 +75,7 @@
             @include('dashboard.layouts.sidebar')
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Welcome back, Dida!</h1>
-                </div>
+                @yield('container')
             </main>
         </div>
     </div>
