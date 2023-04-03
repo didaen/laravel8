1b41fcf2419a31712f54bccf4c5bc2f48dc30f68
Dida En <firdanb@students.unnes.ac.id>
2023-04-03 19:25:26 +0700

    Memotong <nav> pada resources/views/dashboard/layouts/main.blade.php dan menggantinya dengan @include('dashboard.layouts.sidebar') agar lebih rapi dan efisien dalam pemanggilan tampilan layout.

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -72,24 +72,7 @@
 
     <div class="container-fluid">
         <div class="row">
-            <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
-                <div class="position-sticky pt-3 sidebar-sticky">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">
-                            <span data-feather="home" class="align-text-bottom"></span>
-                            Dashboard
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">
-                            <span data-feather="file" class="align-text-bottom"></span>
-                            My Post
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
+            @include('dashboard.layouts.sidebar')
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
