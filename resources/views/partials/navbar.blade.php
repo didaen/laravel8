2997e01a6759642ad2db518dab141f0e0ec223de
Dida En <firdanb@students.unnes.ac.id>
2023-04-03 13:00:00 +0700

    Menambahkan menu dropdown pada resources/views/partials/navbar.blade.php untuk menampilkan menu My Dashboard dan Logout dengan tulisan Welcome back, [nama pengguna]. Menu dropdown dibuat dengan Bootstrap.

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -19,11 +19,26 @@
             <a class="nav-link {{ ($active === "categories") ? 'active' : '' }}" href="/categories">Categories</a>
           </li>
         </ul>
+
         <ul class="navbar-nav ms-auto">
-          <li class="nav-item">
-            <a class="nav-link {{ ($active === "login") ? 'active' : '' }}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
-          </li>
+          @auth
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                Welcome back, {{ auth()->user()->name }}
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">My Dashboard</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Logout</a></li>
+              </ul>
+            </li>
+          @else
+            <li class="nav-item">
+              <a class="nav-link {{ ($active === "login") ? 'active' : '' }}" href="/login"><i class="bi bi-box-arrow-in-right"></i> Login</a>
+            </li>
+          @endauth
         </ul>
+
       </div>
     </div>
 </nav>
