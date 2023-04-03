acf1c2a0d0211b01e6f2e2247034f88d4e108c80
Dida En <firdanb@students.unnes.ac.id>
2023-04-03 19:22:12 +0700

    Mengatur ualng indentasi pada resources/views/dashboard/layouts/main.blade.php.

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -73,22 +73,22 @@
     <div class="container-fluid">
         <div class="row">
             <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-body-tertiary sidebar collapse">
-            <div class="position-sticky pt-3 sidebar-sticky">
-                <ul class="nav flex-column">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">
-                        <span data-feather="home" class="align-text-bottom"></span>
-                        Dashboard
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">
-                        <span data-feather="file" class="align-text-bottom"></span>
-                        My Post
-                        </a>
-                    </li>
-                </ul>
-            </div>
+                <div class="position-sticky pt-3 sidebar-sticky">
+                    <ul class="nav flex-column">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">
+                            <span data-feather="home" class="align-text-bottom"></span>
+                            Dashboard
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">
+                            <span data-feather="file" class="align-text-bottom"></span>
+                            My Post
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
 
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
