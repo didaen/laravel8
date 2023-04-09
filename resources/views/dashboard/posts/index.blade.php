eeaf6923bbac8abf0a3d79af9d2f140a2ce57483
Dida En <firdanb@students.unnes.ac.id>
2023-04-09 12:25:45 +0700

    Membuat tabel yang menampilkan data postingan pengguna yang sedang login. Penampilan nomor postingan memanfaatkan loop->iteration dari Laravel.

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -4,7 +4,7 @@
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
         <h1 class="h2">My Posts</h1>
     </div>
-    <div class="table-responsive">
+    <div class="table-responsive col-lg-8">
         <table class="table table-striped table-sm">
           <thead>
             <tr>
@@ -15,12 +15,14 @@
             </tr>
           </thead>
           <tbody>
+            @foreach ($posts as $post)
             <tr>
-              <td>1,001</td>
-              <td>random</td>
-              <td>data</td>
-              <td>placeholder</td>
+              <td>{{ $loop->iteration }}</td>
+              <td>{{ $post->title }}</td>
+              <td>{{ $post->category->name }}</td>
+              <td>Ubah, Hapus</td>
             </tr>
+            @endforeach
           </tbody>
         </table>
       </div>
