2718cb1d6242b57c1ab75cc0594ecf44855a0e75
Dida En <firdanb@students.unnes.ac.id>
2023-04-09 08:48:08 +0700

    Membuat route untuk menuju ke controller resource DashboardPostController.

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -71,4 +71,7 @@ Route::get('/dashboard', function() {
     return view('dashboard.index');
 })->middleware('auth');
 
+// Membuat DashboardPostController
+// Controller RESOURCE
+Route::resource('photos', DashboardPostController::class)->middleware('auth');
 
