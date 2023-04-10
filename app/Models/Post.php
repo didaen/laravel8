555dac7e38505d24f76210bc42415b639b2aea51
Dida En <firdanb@students.unnes.ac.id>
2023-04-10 16:03:50 +0700

    Menambahkan namespace untuk package Sluggable, trait Sluggable, dan method sluggable()pada app/Models/Post.php untuk membuat slug secara otomatis.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Sluggable;
 
     // protected $fillable = ['title', 'author', 'excerpt', 'body'];
     protected $guarded = ['id'];
@@ -67,5 +68,16 @@ class Post extends Model
     public function getRouteKeyName()
     {
         return 'slug';
+    }
+
+    // METHOD SLUGGABLE
+    // Package untuk membuat slug secara otomatis
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
