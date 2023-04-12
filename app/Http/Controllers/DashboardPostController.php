052e322720c2070f05ddfa338d99e4d983bc682d
Dida En <firdanb@students.unnes.ac.id>
2023-04-12 20:48:51 +0700

    Menambahkan 'user_id' dan 'excerpt' ke validatedData[]. user_id mengambil dari auth()->user()->id. Excerpt memanfaatkan dokumentasi limit untuk Str. Pada excerpt menggunakan strip_tags() PHP untuk menghilangkan tag html agar excerpt ketika ditampilkan tidak mengandung format html. Menggunakan Str::limit() untuk memotong karakter atau membatasi karakter hanya 200 buah. Setelah itu menyimpan data ke Post::create(calidatedData) dan kembalikan ke 'dashboard/posts' dengan menampilkan pesan flash.

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -51,6 +52,20 @@ class DashboardPostController extends Controller
             'category_id' => 'required',
             'body' => 'required'
         ]);
+
+        // user_id
+        $validatedData['user_id'] = auth()->user()->id;
+
+        // excerpt
+        // ... adalah default makanya tidak perlu ditulis
+        // strip_tags() punya PHP untuk menghilangkan semua tag html
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        // Inset ke database
+        Post::create($validatedData);
+
+        // Kembalikan sambil menampilkan flash data
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
