a1f7f5868e476e9547a586f174aeb8aee22b1c7c
Dida En <firdanb@students.unnes.ac.id>
2023-04-13 20:35:56 +0700

    Menduplikat create.blade.php kemudian menggantinya dengan edit.blade.php karena tampilan untuk edit dan create hampir sama.

---
+++ resources/views/dashboard/posts/edit.blade.php
@@ -0,0 +1,74 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Create New Post</h1>
+    </div>
+    <div class="col-lg-8">
+        <form method="post" action="/dashboard/posts" class="mb-5">
+            @csrf
+            <div class="mb-3">
+              <label for="title" class="form-label">Title</label>
+              <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" required autofocus value="{{ old('title') }}">
+              @error('title')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
+            </div>
+            <div class="mb-3">
+              <label for="slug" class="form-label">Slug</label>
+              <input type="text" class="form-control @error('slug') is-invalid @enderror" id="slug" name="slug" required value="{{ old('slug') }}">
+              @error('slug')
+                <div class="invalid-feedback">
+                  {{ $message }}
+                </div>
+              @enderror
+            </div>
+            <div class="mb-3">
+              <label for="category" class="form-label">Category</label>
+              <select class="form-select @error('category_id') is-invalid @enderror" name="category_id">
+                @foreach ($categories as $category)
+                  @if(old('category_id') == $category->id)
+                    <option value="{{ $category->id }}" selected>{{ $category->name }}</option> 
+                  @else
+                    <option value="{{ $category->id }}">{{ $category->name }}</option>
+                  @endif
+                @endforeach
+              </select>
+            </div>
+            <div class="mb-3">
+              <label for="body" class="form-label">Body</label>
+              @error('body')
+                <p class="text-danger">{{ $message }}</p>
+              @enderror
+              <input id="body" type="hidden" name="body" required value="{{ old('body') }}">
+              <trix-editor input="body"></trix-editor>
+            </div>
+            <button type="submit" class="btn btn-primary">Create Post</button>
+          </form>
+    </div>
+    {{-- <a href="/dashboard/posts/checkSlug">Coba klik</a> --}}
+    <script>
+      // Ambil component dengan id title kemudian masukkan ke const title
+      const title = document.querySelector('#title');
+
+      // Ambil component dengan id slug kemudian masukkan ke const slug
+      const slug = document.querySelector('#slug');
+
+      // Event handler untuk menangani ketika ada perubahan atau permintaan slug
+      title.addEventListener('change', function() {
+        fetch('/dashboard/posts/checkSlug?title=' + title.value)
+          .then(response => response.json())
+          .then(data => slug.value = data.slug)
+
+        // console.log('OK');
+      });
+
+      // Menonaktifkan fungsi menambahkan attachment trix editor via JS
+      document.addEventListener('trix-file-accept', function(e) {
+        e.preventDefault();
+      });
+
+    </script>
+@endsection
