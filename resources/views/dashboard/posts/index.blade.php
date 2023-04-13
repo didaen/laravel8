7cc7c2a130bb5fe8254ec0ed71d9c8fd600c695c
Dida En <firdanb@students.unnes.ac.id>
2023-04-13 20:33:33 +0700

    Menambahkan href pada <a> badge edit pada dashboard. href mengarah ke '/dashboard/posts/{{ post->slug }}/edit. Hal ini sesuai dengan aturan route untuk controller resource yang dapat dilihat dengan mengetikkan 'php artisan route:list'. Methodnya PUT/PATCH, pilih satu.

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -33,7 +33,7 @@
                 <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">
                     <span data-feather="eye" class="align-text-bottom"></span>
                 </a>
-                <a href="#" class="badge bg-warning">
+                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning">
                     <span data-feather="edit" class="align-text-bottom"></span>
                 </a>
                 <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
