0bd1036f2b6946ddae636938cb23368d8629bbd5
Dida En <firdanb@students.unnes.ac.id>
2023-04-13 18:49:41 +0700

    Menambahkan attribute onclick untuk mengirimkan pesan sebelum mengahapus sebuah postingan.

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -39,7 +39,7 @@
                 <form action="/dashboard/posts" method="post" class="d-inline">
                   @method('delete')
                   @csrf
-                  <button class="badge bg-danger border-0"><span data-feather="x" class="align-text-bottom"></span></button>
+                  <button class="badge bg-danger border-0"><span data-feather="x" class="align-text-bottom" onclick="return confirm('Are you sure want to delete this post?')"></span></button>
                 </form>
                     
                 </a>
