588f38151c2ed6ccf2bc0494322f9bd6c6878ae8
Dida En <firdanb@students.unnes.ac.id>
2023-04-15 14:52:53 +0700

    Mengomentari bagian pengondisian hak akses pada app/Http/Controllers/AdminCategoryController.php karena dipindahkan ke middleware IsAdmin.

--- app/Http/Controllers/AdminCategoryController.php
+++ app/Http/Controllers/AdminCategoryController.php
@@ -17,11 +17,11 @@ class AdminCategoryController extends Controller
         // Melakukan pengecekan secara manual apakah yang login merupakan admin atau bukan
         // Jika yang membuka view adalah guest ATAU yang mengakses bukan didaen
         // Bisa pake auth()->check() yang jika sudah login menghasilkan TRUE, sehingga pengondisiannya menjadi !user()->check()
-        if(auth()->guest() || auth()->user()->username !== 'didaen') {
+        // if(auth()->guest() || auth()->user()->username !== 'didaen') {
 
             // Gagalkan sambil tampilkan pesan 403 Forbidden
-            abort(403);
-        }
+            // abort(403);
+        // }
 
         // Buka view dashboard/categories/index.blade.php dan kirimkan data
         return view('dashboard.categories.index', [
