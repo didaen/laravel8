4935c503c0318e1067cec064423e61c92439b15b
Dida En <firdanb@students.unnes.ac.id>
2023-04-15 13:31:06 +0700

    Mengedit sintaks yang dicopy dari view 'dashboard/posts/index.blade.php' ke 'dashboard/categories/index.blade.php' agar tidak error dan menyesuaikan dengan menampilkan data category beserta link categories.

--- resources/views/dashboard/categories/index.blade.php
+++ resources/views/dashboard/categories/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
     <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-        <h1 class="h2">My Posts</h1>
+        <h1 class="h2">Post Categories</h1>
     </div>
 
     {{-- Menampilkan pesan flash --}}
@@ -13,30 +13,28 @@
     @endif
 
     <div class="table-responsive col-lg-8">
-      <a href="/dashboard/posts/create" class="btn btn-primary mb-3">Create new post</a>
+      <a href="/dashboard/categories/create" class="btn btn-primary mb-3">Create new category</a>
       <table class="table table-striped table-sm">
         <thead>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Title</th>
-            <th scope="col">Category</th>
+            <th scope="col">Category Name</th>
             <th scope="col">Action</th>
           </tr>
         </thead>
         <tbody>
-          @foreach ($posts as $post)
+          @foreach ($categories as $category)
             <tr>
               <td>{{ $loop->iteration }}</td>
-              <td>{{ $post->title }}</td>
-              <td>{{ $post->category->name }}</td>
+              <td>{{ $category->name }}</td>
               <td>
-                <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info">
+                <a href="/dashboard/categories/{{ $category->slug }}" class="badge bg-info">
                     <span data-feather="eye" class="align-text-bottom"></span>
                 </a>
-                <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning">
+                <a href="/dashboard/categories/{{ $category->slug }}/edit" class="badge bg-warning">
                     <span data-feather="edit" class="align-text-bottom"></span>
                 </a>
-                <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+                <form action="/dashboard/categories/{{ $category->slug }}" method="post" class="d-inline">
                   @method('delete')
                   @csrf
                   <button class="badge bg-danger border-0" onclick="return confirm('Are you sure want to delete this post?')"><span data-feather="x" class="align-text-bottom"></span></button>
